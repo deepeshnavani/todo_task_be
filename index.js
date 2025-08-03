60c63b2815a2ada73ba27c88cc04a5f9420ea738
Deepesh <navanideepesh67@gmail.com>
2025-08-03 12:45:31 +0530

    index changed

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const supabase = require("./src/connection/connect");
 const mainRouter = require("./src/router/route");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://todo-task-fe.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", mainRouter);
@@ -15,6 +20,7 @@ const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
 if (supabase) {
   console.log("Supabase connection created!");
 } else {
